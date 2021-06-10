d07fb7f5936466157f55c3a3639ad8707f72d6f9
bkDezmond <bkDezmond@gmail.com>
2021-06-10 20:39:45 +0300

    задание 4

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const NAMES = ['Игорь', 'Елена', 'Олег', 'Мария', 'Евгений', 'Виктория'];
 const COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 const TOTAL_PHOTOS = 25;
-const AVATAR_NUMBERS = [1, 2, 3 , 4, 5 , 6];
+const AVATAR_NUMBERS = [1, 2, 3, 4, 5, 6];
+const UNIQUE_IDS = new Array(100).fill(null).map((value, i) => i + 1).sort(() => Math.random() - 0.5);
 
 const checkMaxLength = (line, maxLength) => line.length <= maxLength;
 
@@ -16,44 +17,44 @@ const getRandomNumber = (minNumber, maxNumber) => {
   }
   throw new Error('Диапазон может быть только положительный, включая ноль');
 };
-getRandomNumber(1, 25);
 
-const getAvatar = () => {
-  const randomIndex = getRandomNumber(1, AVATAR_NUMBERS.length);
-  const avatarSrc = `img/avatar-${  AVATAR_NUMBERS[randomIndex]  }.svg`;
+const getAvatarSrc = () => {
+  const randomIndex = getRandomNumber(0, AVATAR_NUMBERS.length);
+  const avatarSrc = `img/avatar-${AVATAR_NUMBERS[randomIndex]}.svg`;
   return avatarSrc;
 };
 
-const getPhoto = () => {
-  const photosSrc = `photos/${  getRandomNumber(1, 25)  }.svg`;
+const getPhotoSrc = () => {
+  const photosSrc = `photos/${getRandomNumber(1, 25)}.svg`;
   return photosSrc;
 };
 
-const getRandomName = () => {
-  const randomIndex = getRandomNumber(0, NAMES.length);
-  return NAMES[randomIndex];
+const getRandomItem = (array) => {
+  const randomIndex = getRandomNumber(0, array.length);
+  return array[randomIndex];
 };
 
-const getRandomComment = () => {
-  const randomIndex = getRandomNumber(0, COMMENTS.length);
-  return COMMENTS[randomIndex];
-};
-
-const getSomeComments = () => ({
-  id: getRandomNumber(1, 25),
-  avatar: getAvatar(),
-  message: getRandomComment(),
-  name: getRandomName(),
+const getRandomComment = (id) => ({
+  id,
+  avatar: getAvatarSrc(),
+  message: getRandomItem(COMMENTS),
+  name: getRandomItem(NAMES),
 });
 
-const createPhotos = () => ({
-  id: getRandomNumber(1, 25),
-  url: getPhoto(),
+const getRandomComments = () => {
+  const randomCount = getRandomNumber(0, 5);
+  const uniqueIds = UNIQUE_IDS.splice(0, randomCount);
+  return uniqueIds.map((id) => getRandomComment(id));
+};
+
+const getPhoto = (id) => ({
+  id,
+  url: getPhotoSrc(),
   description: 'description',
   likes: getRandomNumber(15, 200),
-  comments: getSomeComments(),
+  comments: getRandomComments(),
 });
 
-const getPhotosArray = new Array(TOTAL_PHOTOS).fill(null).map(() => createPhotos());
+const photosArray = new Array(TOTAL_PHOTOS).fill(null).map((value, index) => getPhoto(index + 1)).sort(() => Math.random() - 0.5);
 
-getPhotosArray();
+photosArray;
