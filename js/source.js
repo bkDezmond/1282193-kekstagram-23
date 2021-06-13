1fe4158eeaaf712b5b87908c546df56fddd90fa1
bkDezmond <bkDezmond@gmail.com>
2021-06-13 12:28:37 +0300

    module5

---
+++ js/source.js
@@ -0,0 +1,17 @@
+import {getRandomNumber} from './util.js';
+
+
+const AVATAR_NUMBERS = [1, 2, 3, 4, 5, 6];
+
+const getAvatarSrc = () => {
+  const randomIndex = getRandomNumber(0, AVATAR_NUMBERS.length);
+  const avatarSrc = `img/avatar-${AVATAR_NUMBERS[randomIndex]}.svg`;
+  return avatarSrc;
+};
+
+const getPhotoSrc = () => {
+  const photosSrc = `photos/${getRandomNumber(1, 25)}.svg`;
+  return photosSrc;
+};
+
+export {getAvatarSrc, getPhotoSrc};
